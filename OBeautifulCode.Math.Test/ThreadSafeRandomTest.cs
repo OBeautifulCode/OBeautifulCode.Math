9d66cd2a750900abd145cbe72e43acb1ca99d13a
Suraj Gupta <surajg@gmail.com>
2016-03-30 13:40:21 -0400

    cleanup helper methods

--- OBeautifulCode.Math.Test/ThreadSafeRandomTest.cs
+++ OBeautifulCode.Math.Test/ThreadSafeRandomTest.cs
@@ -7,8 +7,8 @@
 namespace OBeautifulCode.Math.Test
 {
     using System;
-    using System.Collections;
     using System.Collections.Concurrent;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
 
     using Xunit;
@@ -16,12 +16,6 @@ namespace OBeautifulCode.Math.Test
     /// <summary>
     /// Tests the <see cref="ThreadSafeRandom"/> class.
     /// </summary>
-    /// <remarks>
-    /// This class was ported from an older library that used a poor style of unit testing.
-    /// It had a few monolithic test methods instead of many smaller, single purpose methods.
-    /// Because of the volume of test code, I was only able to break-up a few of these monolithic tests.
-    /// The rest remain as-is.
-    /// </remarks>
     public static class ThreadSafeRandomTest
     {
         // ReSharper disable InconsistentNaming
@@ -169,47 +163,43 @@ namespace OBeautifulCode.Math.Test
         /// <summary>
         /// Determines if a set of numbers is random.
         /// </summary>
-        /// <param name="toEvaluate">The random numbers to test.</param>
-        /// <param name="r">The the size of the range of numbers.</param>
+        /// <param name="numbers">The random numbers to test.</param>
+        /// <param name="range">The the size of the range of numbers.</param>
         /// <remarks>
         /// See here for algorithm: <a href="https://en.wikibooks.org/wiki/Algorithm_Implementation/Pseudorandom_Numbers/Chi-Square_Test"/>.
         /// </remarks>
         /// <returns>
         /// Returns true if the set of numbers is random, false if not.
         /// </returns>
-        private static bool IsRandom(int[] toEvaluate, int r)
+        private static bool IsRandom(int[] numbers, int range)
         {
+#pragma warning disable SA1305 // Field names must not use Hungarian notation
             // Calculates the chi-square value for N positive integers less than r
             // Source: "Algorithms in C" - Robert Sedgewick - pp. 517
             // NB: Sedgewick recommends: "...to be sure, the test should be tried a few times,
             // since it could be wrong in about one out of ten times."
 
             // Calculate the number of samples - N
-            int n = toEvaluate.Length;
+            int n = numbers.Length;
 
             // According to Sedgewick: "This is valid if N is greater than about 10r"
-            if (n <= 10 * r)
+            if (n <= 10 * range)
             {
                 return false;
             }
 
-            // ReSharper disable PossibleLossOfFraction
-#pragma warning disable SA1305 // Field names must not use Hungarian notation
-            double nR = n / r;
-#pragma warning restore SA1305 // Field names must not use Hungarian notation
-            // ReSharper restore PossibleLossOfFraction
-
+            double nR = Convert.ToDouble(n) / Convert.ToDouble(range);
             double chiSquare = 0;
 
             // PART A: Get frequency of randoms
-            Hashtable ht = RandomFrequency(toEvaluate);
+            var frequencies = CalculateFrequency(numbers);
 
             // PART B: Calculate chi-square - this approach is in Sedgewick
             // ReSharper disable LoopCanBeConvertedToQuery
 
-            foreach (DictionaryEntry item in ht)
+            foreach (var frequency in frequencies.Values)
             {
-                double f = (int)item.Value - nR;
+                double f = frequency - nR;
                 chiSquare += Math.Pow(f, 2) / nR;
             }
 
@@ -217,39 +207,34 @@ namespace OBeautifulCode.Math.Test
 
             // PART C: According to Swdgewick: "The statistic should be within 2(r)^1/2 of r
             // This is valid if N is greater than about 10r"
-            if ((r - (2 * Math.Sqrt(r)) <= chiSquare) & (r + (2 * Math.Sqrt(r)) >= chiSquare))
-            {
-                return true;
-            }
-
-            return false;
+            var result = (range - (2 * Math.Sqrt(range)) <= chiSquare) & (range + (2 * Math.Sqrt(range)) >= chiSquare);
+            return result;
+#pragma warning restore SA1305 // Field names must not use Hungarian notation
         }
 
         /// <summary>
-        /// Gets the frequency of occurrence of a randomly generated array of integers
+        /// Calculates the frequency of occurrence of each integer in a set of integers.
         /// </summary>
-        /// <param name="randomNums">The random numbers to test.</param>
+        /// <param name="numbers">The integers to count.</param>
         /// <returns>
-        /// A hash table, key being the random number and value its frequency.
+        /// A dictionary where keys are the numbers observed and values are their frequency.
         /// </returns>
-        private static Hashtable RandomFrequency(int[] randomNums)
+        private static IDictionary<int, int> CalculateFrequency(int[] numbers)
         {
-            var ht = new Hashtable();
-            int n = randomNums.Length;
-
-            for (int i = 0; i <= n - 1; i++)
+            var result = new Dictionary<int, int>();
+            foreach (int number in numbers)
             {
-                if (ht.ContainsKey(randomNums[i]))
+                if (result.ContainsKey(number))
                 {
-                    ht[randomNums[i]] = (int)ht[randomNums[i]] + 1;
+                    result[number] = result[number] + 1;
                 }
                 else
                 {
-                    ht[randomNums[i]] = 1;
+                    result[number] = 1;
                 }
             }
 
-            return ht;
+            return result;
         }
 
         // ReSharper restore InconsistentNaming
